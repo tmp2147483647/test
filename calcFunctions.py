2587f3def023fb597bb5afc8eeec600a15efa414
tmp2147483647 <reverse@kookmin.ac.kr>
2017-11-13 21:09:42 +0900

    Add files via upload

---
+++ calcFunctions.py
@@ -0,0 +1,47 @@
+from math import factorial as fact
+
+
+def factorial(numStr):
+    try:
+        n = int(numStr)
+        r = str(fact(n))
+    except:
+        r = 'Error!'
+    return r
+
+def decToBin(numStr):
+    try:
+        n = int(numStr)
+        r = bin(n)[2:]
+    except:
+        r = 'Error!'
+    return r
+
+def binToDec(numStr):
+    try:
+        n = int(numStr, 2)
+        r = str(n)
+    except:
+        r = 'Error!'
+    return r
+
+def decToRoman(numStr):
+    return 'dec -> Roman'
+
+
+functionMap = [
+    ('factorial (!)', factorial),
+    ('-> binary', decToBin),
+    ('binary -> dec', binToDec),
+    ('-> roman', decToRoman),
+    ('roman -> dec', decToRoman),
+]
+
+
+functionMap2 = {
+    'factorial (!)': factorial,
+    '-> binary': decToBin,
+    'binary -> dec': binToDec,
+    '-> roman': decToRoman,
+    'roman -> dec': decToRoman,
+}
